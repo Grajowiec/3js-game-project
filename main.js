bcc306bff0cff0f61fa93fda4834ff802d7cdedb
Grajowiec <raymanfan1@interia.pl>
2024-05-12 13:50:51 +0200

    Added collisions to barriers & general improvements in the comments and code itself

--- main.js
+++ main.js
@@ -29,21 +29,21 @@ const barrier4 = new THREE.Mesh( geometry_barrier, material_barrier );
 scene.add( barrier4 );
 barrier4.position.x =  5;
 barrier4.position.y = -1.1;
-//^^ creating and adding barriers
+//^^ Creating and adding barriers
 
 const geometry_player = new THREE.BoxGeometry();
 const material_player = new THREE.MeshBasicMaterial( { color: 0x0000FF } );
 const player = new THREE.Mesh( geometry_player, material_player );
 scene.add( player );
 player.position.y = -2.85;
-//^^ creates and spawns player ship
+//^^ Creates and spawns player ship
 
 const geometry_bullet = new THREE.SphereGeometry(0.12);
 const material_bullet = new THREE.MeshBasicMaterial( { color: 0xFF0000 } );
 const bullet = new THREE.Mesh(geometry_bullet,material_bullet)
-//^^ building bullets
+//^^ Creating bullets
 
-const bullets = []; // Store bullets in an array so it won't disappear
+const bullets = []; // Stores bullets in an array so it won't disappear
 
 camera.position.z = 5;
 
@@ -51,43 +51,55 @@ function shoot() {
 	bullet.position.x = player.position.x
 	bullet.position.y = player.position.y
 	scene.add( bullet );
-	bullets.push(bullet); // Add the bullet to the array so it won't disappear
+	bullets.push(bullet); // Adds bullet to the array so it won't disappear
 }
 
-function key(e) {
+function keyboard(e) {
 	switch (e.keyCode) {
-		case 37: // left
-			player.position.x -= 0.1;
+		case 37: // Left
+			player.position.x -= 0.5;
 			break;
-/*		case 38: // up
+/*		case 38: // Up
 			player.position.y += 0.1;
 			break;
-*/		case 39: // right
-			player.position.x += 0.1;
+*/		case 39: // Right
+			player.position.x += 0.5;
 			break;
-/*		case 40: // down
+/*		case 40: // Down
 			player.position.y -= 0.1;
 			break;
-*/		case 32: //spacebar to shoot
+*/		case 32: // Spacebar to shoot
 			shoot();
 			break;
 		default:
 			break;
 	}
-} //keyboard
+} // Keyboard
 
-addEventListener("keyup", key); //controls
+addEventListener("keyup", keyboard); // Controls
 
 function animate() {
 	requestAnimationFrame( animate );
+	
 	player.rotation.x += 0.01;
 	player.rotation.y += 0.01;
-    // Update bullet positions
+
+    // Updates bullet positions
     bullets.forEach(bullet => {
         bullet.position.y += 0.1; // Bullet speed
-        if (bullet.position.y > 5) { // Remove bullets when they go out of the screen
+
+		// Checking for collision with barriers
+		const barriers = [barrier, barrier2, barrier3, barrier4];
+		barriers.forEach(barrier => {
+			if (bullet.position.distanceTo(barrier.position) < 0.98) { // Collision distance 
+				scene.remove(bullet);
+				bullets.splice(bullets.indexOf(bullet), 1);
+			}
+		});
+
+        if (bullet.position.y > 5) { // Removes bullets when they go out of the screen
             scene.remove(bullet);
-            bullets.splice(bullets.indexOf(bullet), 1); //removes bullet from array when they are out from the screen
+            bullets.splice(bullets.indexOf(bullet), 1); // Removes bullet from array when they are out from the screen
         }
     });
 
